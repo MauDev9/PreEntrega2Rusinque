3b4e59cc6dd97e853fda841e3e39d5ba5f74ea32
Mauricio Rusinque <merc2717@gmail.com>
2023-11-22 19:02:19 -0500

    Se agregan funciones para funcionamineto de algunos botones y se corrigen pequeños detalles del index

--- js/script.js
+++ js/script.js
@@ -1,29 +1,71 @@
 
-// alert("Bienvenidos a la Pizzeria de Mauricio !")
+let carrito = [];
 
-
-
-/*let pizzas = document.getElementsByClassName("card");
-console.log(pizzas[5].innerHTML);*/
-
-/*function agregarAlCarrito(){
-
-};*/
+let pizzas = [
+    { nombre: "Pizza de peperoni", valor: 17000, stock: 5 },
+    { nombre: "Pizza de hongos", valor: 16000, stock: 5 },
+    { nombre: "Pizza de parrilla", valor: 18000, stock: 5 },
+    { nombre: "Pizza de queso", valor: 19000, stock: 5 },
+    { nombre: "Pizza de tomate", valor: 20000, stock: 5 },
+    { nombre: "Pizza de tocino", valor: 17500, stock: 5 },
+];
 
 let ingredientes = [
     "Queso mozzarella ", "Tomate fresco ", "Pepperoni ", "Champiñones ", "Pimientos ",
-"Aceitunas ", "Jamón ", "Cebolla ", "Albahaca ", "Salsa de tomate "
-]
-console.log(ingredientes)
+    "Aceitunas ", "Jamón ", "Cebolla ", "Albahaca ", "Salsa de tomate "
+];
+
+//
+
+//Mediante este bloque de codigo podremos utilizar la barra de busqueda para filtrar la busqueda de una pizza por su nombre exacto.
+const searchBar = document.getElementById('search-bar');
+const searchButton = document.getElementById('search-button');
+searchBar.addEventListener('input', filtrarPizzas);
+searchButton.addEventListener('click', buscarPizzas);
+
+function filtrarPizzas() {
+    const textoBusqueda = searchBar.value.toLowerCase();
+    const pizzasFiltradas = pizzas.filter(pizza => pizza.nombre.toLowerCase().includes(textoBusqueda));
+    mostrarPizzasFiltradas(pizzasFiltradas);
+}
+
+function mostrarPizzasFiltradas(pizzasFiltradas) {
+    console.log(pizzasFiltradas);
+}
+
+function buscarPizzas() {
+    const textoBusqueda = searchBar.value.toLowerCase();
+    const pizzasFiltradas = pizzas.filter(pizza => pizza.nombre.toLowerCase().includes(textoBusqueda));
+
+    if (pizzasFiltradas.length > 0) {
+        const disponibilidad = pizzasFiltradas.map(pizza => `${pizza.nombre} - Disponible`).join('\n');
+        alert(`Si está disponible:\n${disponibilidad}`);
+    } else {
+        alert(`Lo sentimos :/ , no tenemos: ${textoBusqueda}`);
+    }
+}
+
+
+//Bloque de codigo para agregar pizzas al carrito.
+const botonesOrdenar = document.querySelectorAll('.button-agg-carrito');
+    
+    botonesOrdenar.forEach(function(boton, index) {
+        boton.addEventListener('click', function() {
+            agregarAlCarrito(pizzas[index]);
+        });
+    });
 
 
 
+function agregarAlCarrito(pizza) {
+    if (pizza.stock > 0) {
+        carrito.push(pizza);
+        pizza.stock--;
+        alert(`¡${pizza.nombre} ha sido agregada al carrito!`);
+    } else {
+        alert(`Lo sentimos :/ , no hay stock disponible para ${pizza.nombre}`);
+    }
+}
 
-const buildButton = document.getElementsByClassName('build-button');
 
-buildButton.addEventListener('click', function armarPizza() {
 
-    alert("Hola, asi que vas a armar tu propia pizza." + '\n'  + "A continaución te mostraré todos los ingredientes dispponibles "+ '\n' + ingredientes)
-});
-
-armarPizza();
